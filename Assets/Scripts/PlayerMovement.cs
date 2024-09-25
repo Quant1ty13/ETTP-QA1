cad8de9d0225fa594c5c65cbb165611251a83065
Nova Caster <casanovawarleyne@gmail.com>
2024-09-25 21:39:01 +0800

    Jump Input
    Added Jump input in controls.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     int PlayerSpeed = 5;
+
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         if (Input.GetKey(KeyCode.D))
@@ -14,6 +21,11 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-transform.right * Time.deltaTime * PlayerSpeed);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * 9, ForceMode2D.Impulse);
         }
     }
 }
