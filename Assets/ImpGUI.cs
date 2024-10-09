3b4e17274a70d97ac64a8323a3ce25c636fac711
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-10-09 10:46:33 +0800

    warleyne animation button

---
+++ Assets/ImpGUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpGUI : MonoBehaviour
+{
+    public Animator animator;
+    public void UIAnimation()
+    {
+        animator.SetTrigger("StartAnim");
+    }
+}
