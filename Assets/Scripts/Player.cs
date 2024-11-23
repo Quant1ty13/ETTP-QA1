10b1e7b98485a5b9544c97f09e35571888ce0b4e
Nova Caster <casanovawarleyne@gmail.com>
2024-11-24 02:42:43 +0800

    PT2_Damage
    Damage System

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : PlayerStat
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            TrapDamageScript trap = collision.gameObject.GetComponent<TrapDamageScript>();
+
+            if(trap != null)
+            {
+                playerHealth -= trap.trapDamage;
+
+                if (playerHealth < 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
