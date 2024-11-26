907f83830c5fcef420470f498a60f96be974cfaa
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-11-26 15:19:40 +0800

    dashing - last update (if it doesn't break)
    - added dashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    bool Dashing = false;
+    bool canDash = true;
+    string whatCooldown;
+    string dir;
+    public float DashPower;
+
     int PlayerSpeed = 5;
     public PlayerController playerInputs;
     bool CanDoubleJump = false;
@@ -39,7 +46,10 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         movement = playerInputs.Action.Movement.ReadValue<Vector2>();
-        transform.Translate(new Vector2(movement.x, movement.y) * Time.deltaTime * PlayerSpeed);
+        if (Dashing == false)
+        {
+            transform.Translate(new Vector2(movement.x, movement.y) * Time.deltaTime * PlayerSpeed);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -60,24 +70,74 @@ public class PlayerMovement : MonoBehaviour
                     break;
             }
         }
+
+        if (canDash == true && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                dir = "right";
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                dir = "left";
+            }
+
+            Dash();
+        }
+    }
+
+    private void Dash()
+    {
+        Dashing = true;
+        canDash = false;
+        whatCooldown = "dash";
+        rb2d.gravityScale = 0;
+        switch (dir)
+        {
+            case "left":
+                rb2d.velocity = new Vector2(-DashPower, 0);
+                break;
+            case "right":
+                rb2d.velocity = new Vector2(DashPower, 0);
+                break;
+        }
+        dir = "";
+        StartCoroutine(Cooldown());
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Dashing = false;
+        rb2d.gravityScale = 1.6f;
         hasJumped = false;
         TR.emitting = false;
 
         if (collision.gameObject.CompareTag("Trap"))
         {
             spriteRender.color = Color.red;
-            StartCoroutine(HurtChange());
+            whatCooldown = "hurtchange";
+            StartCoroutine(Cooldown());
         }
 
     }
 
-    IEnumerator HurtChange()
+    IEnumerator Cooldown() // in hindsight this could've been an interface if i wasn't an idiot !!
     {
-        yield return new WaitForSeconds(0.5f);
-        spriteRender.color = Color.yellow;
+        switch (whatCooldown)
+        {
+            case "hurtchange":
+                yield return new WaitForSeconds(0.5f);
+                spriteRender.color = Color.yellow;
+                break;
+            case "dash":
+                yield return new WaitForSeconds(0.5f);
+                Dashing = false;
+                rb2d.gravityScale = 1.6f;
+                yield return new WaitForSeconds(1.5f);
+                canDash = true;
+                break;
+        }
+        whatCooldown = "";
+
     }
 }
