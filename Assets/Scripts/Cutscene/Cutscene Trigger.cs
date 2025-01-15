81f5c194366bfe92d1290a2d116d61d0f42530bd
Marcus Ureta <silvergreat0@gmail.com>
2025-01-15 21:30:22 +0800

    forgor to save a script

--- Assets/Scripts/Cutscene/Cutscene Trigger.cs
+++ Assets/Scripts/Cutscene/Cutscene Trigger.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CutsceneTrigger : MonoBehaviour
 {
     [SerializeField] private PlayerHandler playerhandler;
     [SerializeField] private GameObject cutsceneUI;
+    [SerializeField] private PlayableDirector cutsceneClip;
+
+    private void Awake()
+    {
+        cutsceneClip.Play();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         cutsceneUI.SetActive(true);
         playerhandler.enabled = false;
     }
